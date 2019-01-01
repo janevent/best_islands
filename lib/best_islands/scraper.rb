0721727f5fcd26da9782f6a5834c3607493989d5
Jane Venter <janeanneventer@gmail.com>
2019-01-01 01:20:42 +0000

    add loops to #scrape_island_attributes

--- lib/best_islands/scraper.rb
+++ lib/best_islands/scraper.rb
@@ -2,16 +2,24 @@ require 'pry'
 class Scraper
   def scrape_page
     html = open("https://www.islands.com/top-20-best-islands-to-live-on/")
-    doc = Nokogiri::HTML(html)
+    @doc = Nokogiri::HTML(html)
   end
   
   def scrape_island_attributes
-    names = doc.css(".caption span").text
+    names = scrape_page.css(".caption span")
     binding.pry
     names.each do |name|
       island = Island.new 
-      island.island_name = name
+      island.island_name = name.text
     end
-      
+    lists = scrape_page.css(".list")
+    lists.each do |list|
+      list.css("li").each do |item|
+        if item.include?("population")
+          island.population = item.text.split(': ')[1]
+        end
+      end
+    end
   end
+  
 end
