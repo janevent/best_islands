5328b6b598086d4cb61e007342a1a2da7a476459
Jane Venter <janeanneventer@gmail.com>
2019-01-02 05:56:24 +0000

    scrape attributes for instances of the Island class

--- lib/best_islands/scraper.rb
+++ lib/best_islands/scraper.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 class Scraper
   def scrape_page
     html = open("https://www.islands.com/top-20-best-islands-to-live-on/")
@@ -11,9 +11,23 @@ class Scraper
       island = Island.new 
       island.island_name = name.text
     end
-    scrape_page.css(".list li").each do |fact|
-      if fact.text.include?("population")
-        island.population = fact.text.split(': ')[1]
+    scrape_page.css(".list").each_with_index do |list, index|
+      # binding.pry
+      list[index].css("li").each_with_index do |fact, index|
+        if fact.text.include?("Population")
+          island.population = fact.text.split(": ")[1]
+        elsif fact.text.include?("Language")
+          island.language = fact.text.split(": ")[1]
+        elsif fact.text.include?("rent")
+          island.average_rent = fact.text.split(": ")[1]
+        elsif fact.text.include?("home")
+          island.home_price = fact.text.split(": ")[1]
+        elsif fact.text.include?("milk")
+          island.cost_of_milk = fact.text.split(": ")[1]
+        end
+      #if fact.text.include?("population")
+        #island.population = fact.text.split(': ')[1]
+      #end
       end
     end
   end
