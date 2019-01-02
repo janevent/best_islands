a986e5f6e3ab0d83ebda33830f07bb97a52c66b1
Jane Venter <janeanneventer@gmail.com>
2019-01-02 18:27:20 +0000

    write #list_island_names

--- lib/best_islands/cli.rb
+++ lib/best_islands/cli.rb
@@ -12,8 +12,8 @@ class CLI
       if input == "islands"
         list_island_names 
       end
-      puts  "If you would like to see a list of facts about an island type the island's id number."
-      if input.to_i > 0 && input.to_i < Island.all.size
+      puts  "If you would like to see a list of facts about an island type the island id number."
+      if input.to_i > 0 && input.to_i < Island.all.length
         list_island_facts(input)
       end
       puts "If you would like to exit, type "exit".
@@ -23,5 +23,22 @@ class CLI
       input = gets.chomp
     end
   end
+  
+  def list_island_names
+    Island.all.each_with_index do |island, index|
+      puts "#{index + 1}. #{island.island_name}"
+    end
+  end
+  
+  def list_island_facts(id)
+    isl = Island.all[id.to_i -1]
+    puts "#{id}. #{isl.island_name}."
+    puts "Population: #{isl.population}"
+    puts "Language spoken: #{isl.language_spoken}"
+    puts "The average rent for a one bedroom apartment: #{average_rent}"
+    puts "The starting price for a home: #{isl.home_price}"
+    puts "The cost of a gallon of milk: #{isl.cost_of_milk}"
+  end
+    
   
 end
